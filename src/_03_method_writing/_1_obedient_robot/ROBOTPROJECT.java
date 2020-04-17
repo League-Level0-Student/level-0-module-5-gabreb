3407a4f96027fa6a31d4cc5ce4c82be5e88c9875
gabreb <62906858+gabreb@users.noreply.github.com>
2020-04-16 17:32:44 -0700

    hi

--- src/_03_method_writing/_1_obedient_robot/ROBOTPROJECT.java
+++ src/_03_method_writing/_1_obedient_robot/ROBOTPROJECT.java
@@ -11,6 +11,7 @@ public static void main(String[] args) {
 	rob.penDown();
 	rob.setPenColor(Color.red);
 	rob.setSpeed(6);
+	
 }
 
 
@@ -19,8 +20,16 @@ private void drawSquare() {
 	rob.move(100);
 	rob.turn(90);
 	}
-	
 }
+private void drawTriangle() {
+	rob.turn(30);
+	for (int i = 0; i < 3; i++) {
+		rob.move(100);
+		rob.turn(120);
+	}
+}
+	
+
 
 
 
